f8cc6d97aa93367d945462372c88c245ffcf0882
Nytra <14206961+Nytra@users.noreply.github.com>
2024-03-05 08:12:02 +0000

    Make the fix apply to every user in the session instead of just locally

--- NodeAttachmentIssuesHotline/NodeAttachmentIssuesHotline.cs
+++ NodeAttachmentIssuesHotline/NodeAttachmentIssuesHotline.cs
@@ -1,8 +1,10 @@
 using FrooxEngine;
 using FrooxEngine.ProtoFlux;
+using FrooxEngine.ProtoFlux.Runtimes.Execution.Nodes;
 using HarmonyLib;
 using ResoniteModLoader;
 using System;
+using System.Linq;
 using System.Reflection;
 //using ResoniteHotReloadLib;
 
@@ -19,6 +21,8 @@ namespace NodeAttachmentIssuesHotline
 
 		[AutoRegisterConfigKey]
 		static ModConfigurationKey<bool> MOD_ENABLED = new ModConfigurationKey<bool>("MOD_ENABLED", "Mod Enabled:", () => true);
+
+		const string LinkNodeIndentifier = "NodeAttachmentIssuesHotline_Link";
 
 		public override void OnEngineInit()
 		{
@@ -50,17 +54,70 @@ namespace NodeAttachmentIssuesHotline
 			return element != null && !element.IsRemoved;
 		}
 
-		//[HarmonyPatch(typeof(Userspace), "OnCommonUpdate")]
-		//class HotReloadPatch
+		static void RebuildGroupWithLinkNode(ProtoFluxNode node)
+		{
+			if (!ElementExists(node))
+			{
+				Debug("Node was null in CreateLink method.");
+				return;
+			}
+
+			// Create Link node, attach node, wait, destroy link node
+			var linkNodeSlot = node.LocalUserRoot?.Slot?.AddSlot(LinkNodeIndentifier);
+			if (linkNodeSlot == null)
+			{
+				Debug("Link node slot was null after add slot!");
+				return;
+			}
+			var linkNode = linkNodeSlot.AttachComponent<Link>();
+			if (linkNode == null)
+			{
+				Debug("Link was null after attach!");
+				linkNodeSlot.Destroy();
+				return;
+			}
+			Debug($"Link attached: {linkNode.Name ?? "NULL"} {linkNode.ReferenceID.ToString() ?? "NULL"}");
+			linkNode.A.Target = (FrooxEngine.ProtoFlux.Core.INode)node;
+			Debug($"Link node target set to Node: {node.Name ?? "NULL"} {node.ReferenceID.ToString() ?? "NULL"}");
+			linkNode.RunInUpdates(3, () =>
+			{
+				if (ElementExists(linkNode))
+				{
+					linkNode.A.Target = null;
+					Debug("Link node target set to null");
+					linkNode.RunInUpdates(3, () =>
+					{
+						if (ElementExists(linkNode))
+						{
+							Slot linkNodeSlot = linkNode.Slot;
+							linkNode.Destroy(sendDestroyingEvent: false);
+							Debug("Link component destroyed");
+							if (linkNodeSlot.ComponentCount == 0)
+							{
+								linkNodeSlot.Destroy();
+								Debug("Link slot destroyed");
+							}
+						}
+						else
+						{
+							Debug("Link node does not exist in second runInUpdates");
+						}
+					});
+				}
+				else
+				{
+					Debug("Link node does not exist in first runInUpdates");
+				}
+			});
+		}
+
+		//static void RebuildGroupLocally(ProtoFluxNode node)
 		//{
-		//	static void Postfix()
-		//	{
-		//		if (!Config.GetValue(MOD_ENABLED)) return;
-		//		if (Engine.Current.InputInterface.GetKeyDown(Key.F3))
-		//		{
-		//			HotReloader.HotReload(typeof(NodeAttachmentIssuesHotline));
-		//		}
-		//	}
+		//	if (!ElementExists(node)) return;
+		//	if (!ElementExists(node.Slot)) return;
+		//	if (!ElementExists(node.Slot.World)) return;
+		//	if (node.Group == null) return;
+		//	node.World.ProtoFlux.ScheduleGroupRebuild(node.Group);
 		//}
 
 		[HarmonyPatch(typeof(ProtoFluxInputProxy))]
@@ -81,8 +138,8 @@ namespace NodeAttachmentIssuesHotline
 					{
 						try
 						{
-							Debug("Scheduling group rebuild for group: " + node.Group.Name ?? "NULL");
-							__instance.World.ProtoFlux.ScheduleGroupRebuild(node.Group);
+							Debug("Running link node group rebuild for node: " + node.Name ?? "NULL");
+							RebuildGroupWithLinkNode(node);
 						}
 						catch (Exception e)
 						{
@@ -112,8 +169,8 @@ namespace NodeAttachmentIssuesHotline
 					{
 						try
 						{
-							Debug("Scheduling group rebuild for group: " + node.Group.Name ?? "NULL");
-							__instance.World.ProtoFlux.ScheduleGroupRebuild(node.Group);
+							Debug("Running link node group rebuild for node: " + node.Name ?? "NULL");
+							RebuildGroupWithLinkNode(node);
 						}
 						catch (Exception e)
 						{
@@ -133,17 +190,25 @@ namespace NodeAttachmentIssuesHotline
 			{
 				if (!Config.GetValue(MOD_ENABLED)) return true;
 				if (!ElementExists(__instance)) return true;
+				if (!Engine.Current.IsReady) return true;
 				if (__instance.Group?.Nodes == null) return true;
 				Debug($"OnDestroying ProtoFluxNode: {__instance.Name ?? "NULL"} {__instance.ReferenceID.ToString() ?? "NULL"} Group: {__instance.Group.Name ?? "NULL"}");
-				try
+				ProtoFluxNodeGroup group = __instance.Group;
+
+				// wait some updates just in case the rest of the nodes in the group are also being destroyed
+				// not sure how many updates to wait but 1 seems ok, could maybe try 3 if there are issues
+				__instance.World.RunInUpdates(1, () =>
 				{
-					//Debug("Scheduling group rebuild for group: " + node.Group.Name ?? "NULL");
-					__instance.World.ProtoFlux.ScheduleGroupRebuild(__instance.Group);
-				}
-				catch (Exception e)
-				{
-					Error("Exception while scheduling group rebuild:\n" + e.ToString());
-				}
+					if (group != null && group.NodeCount > 0)
+					{
+						var node = group.Nodes.FirstOrDefault(node => ElementExists(node));
+						if (node != null)
+						{
+							Debug("Running link node group rebuild for node: " + node.Name ?? "NULL");
+							RebuildGroupWithLinkNode(node);
+						}
+					}
+				});
 				return true;
 			}
 		}
